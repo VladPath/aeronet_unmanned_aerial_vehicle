b6fedd3fa8b45dcd976c0c223f7f9c33ccc97b35
Vladislav <146530026+VladPath@users.noreply.github.com>
2025-01-14 16:02:14 +0300

    add bootstrap base,style,views

--- aeronet/BAS/views.py
+++ aeronet/BAS/views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+data_db = [
+    {'id': 1, 'title': 'Геоскан LITE', 'content': '''Геоскан LITE информация.''','img':'BAS/images/background_1.jpg',
+     'is_published': True},
+    {'id': 2, 'title': 'Геоскан MAX', 'content': 'Геоскан MAX информация', 'img':'BAS/images/background_1.jpg', 'is_published': False},
+    {'id': 3, 'title': 'Геоскан STANDART', 'content': 'Геоскан STANDART Информация', 'img':'BAS/images/background_1.jpg', 'is_published': True},
+]
+
+
 # Create your views here.
-def index(response):
-    return HttpResponse('Hello World')
+def index(request):
+    
+    data = {
+        'title':'Каталог Российских моделей БАС',
+        'data': data_db,
+    }
+    return render(request, 'BAS/index.html', context=data)
+
+
 
